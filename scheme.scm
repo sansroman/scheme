3e0c26d799ddd32b77b7f19a8a86c11ef25561e9
tboevil <tboevil@gmail.com>
2018-08-31 11:35:52 +0800

    complete chapter 7

--- scheme.scm
+++ scheme.scm
@@ -152,7 +152,7 @@
     ((null? tup) 0)
     (else (o+ (car tup) (addtup (cdr tup)))))))
 
-(define x
+(define o*
   (lambda (n m)
   (cond
     ((zero? m) 0)
@@ -166,7 +166,169 @@
     (else (cons (o+ (car tup1) (car tup2))
                 (tup+ (cdr tup1) (cdr tup2)))))))
 
-(define expt
+
+(define quotient
   (lambda (n m)
   (cond
-    (zero? m) ))
+    ((< n m)0)
+    (else (add1 (quotient (o- n m)m))))))
+(define length
+  (lambda (lat)
+  (cond
+    ((null? lat) 0)
+    (else (add1 (length (cdr lat)))))))
+    
+(define pick
+      (lambda (n lat)
+      (cond
+        ((zero? (sub1 n))(car lat))
+        (else (pick (sub1 n)(cdr lat))))))
+(define rempick
+  (lambda (n lat)
+  (cond
+    ((zero? (sub1 n))(cdr lat))
+    (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+  (cond
+    ((null? lat) '())
+    ((number? (car lat)) (no-nums (cdr lat)))
+    (else (cons (car lat) (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+  (cond
+    ((null? lat) '())
+    ((number? (car lat)) (cons (car lat) (all-nums(cdr lat))))
+    (else (all-nums (cdr lat))))))
+
+(define eqan?
+  (lambda (n1 n2)
+  (cond
+    ((and (number? n1) (number? n2)) (= n1 n2))
+    ((or (number? n1) (number? n2)) #f)
+    (else (eq? n1 n2)))))
+
+(define occur
+  (lambda (a lat)
+  (cond
+    ((null? lat) 0)
+    ((eqan? (car lat) a) (add1 (occur a (cdr lat))))
+    (else (occur a (cdr lat))))))
+(define one?
+  (lambda (n)
+  (= n 1)))
+
+(define rember*
+  (lambda (a lat)
+  (cond
+    ((null? l) '())
+    ((atom? (car l)) 
+      (cond
+        ((eq? (car l) a) (rember* a (cdr l)))
+        (else (cons (car l)(rember* a (cdr l))))))
+    (else (cons (rember* a (car l)) (rember* a (cdr l)))))))
+
+(define insertR*
+  (lambda (new old l)
+  (cond
+    ((null? l) '())
+    ((atom? (car l)) 
+      (cond
+        ((eq? (car l) old) (cons (car l) (cons new (insertR* new old (cdr l)))))
+        (else (cons (car l) (insertR* new old (cdr l))))))
+    (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
+
+(define occur*
+  (lambda (a l)
+  (cond
+    ((null? l) 0)
+    ((atom? (car l)) 
+      (cond
+        ((eq? (car l) a) (add1 (occur* a (cdr l))))
+        (else (occur* a (cdr l)))))
+    (else (o+ (occur* a (car l)) (occur* a (cdr l)))))))
+
+(define subst*
+  (lambda (new old l)
+  (cond
+    ((null? l) '())
+    ((atom? (car l)) 
+      (cond
+        ((eq? (car l) old) (cons new (subst* new old (cdr l))))
+        (else (cons (car l) (subst* new old (cdr l))))))
+    (else (cons (subst* new old (car l)) (subst* new old (cdr l)))))))
+
+(define insertL*
+  (lambda (new old l)
+  (cond 
+    ((null? l) '())
+    ((atom? (car l))
+      (cond
+        ((eq? (car l) old) (cons new l))
+        (else (cons (car l) (insertL* new old (cdr l))))))
+    (else (cons (insertL* new old (car l)) (insertL* new old (cdr l)))))))
+
+(define member*
+  (lambda (a l)
+  (cond
+    ((null? l) #f)
+    ((atom? (car l))
+      (cond
+        ((eq? (car l) a) #t)
+        (else (member* a (cdr l)))))
+    (else (or (member* a (car l)) (member* a (cdr l)))))))
+  
+(define leftmost
+  (lambda (l)
+  (cond
+    ((atom? (car l)) (car l))
+    (else (leftmost (car l))))))
+
+(define eqlist?
+  (lambda (l1 l2)
+  (cond 
+    ((and (null? l1) (null? l2)) #t)
+    ((or (null? l1) (null? l2)) #f)
+    ((and (atom? (car l1)) (atom? (car l2))) (and (eq? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+    ((or (atom? (car l1)) (atom? (car l2))) #f)
+    (else (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))))))
+
+(define equal?
+  (lambda (s1 s2)
+  (cond
+    ((and (atom? s1)(atom? s2)) (eq? s1 s2))
+    ((or (atom? s1) (atom? s2)) #f)
+    (else (eqlist? s1 s2)))))
+(define numbered?
+  (lambda (aexp)
+  (cond
+    ((atom? aexp)(number? aexp))
+    (else
+      (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp)))))))))
+(define value
+  (lambda (nexp)
+  (cond
+    ((atom? nexp) nexp)
+    ((eq? (car (cdr nexp)) 'o+) o+ (value (car nexp)))
+    ((eq? (car (cdr nexp)) 'o*) o* (value (car nexp)))
+    ((eq? (car (cdr nexp)) 'o^) o^ (value (car nexp)))
+    )))
+  
+(define lst-sub-exp
+  (lambda (aexp) (car (cdr aexp))))
+
+(define 2nd-sub-exp
+  (lambda (aexp) (car (cdr (cdr aexp)))))
+
+(define operator
+  (lambda (aexp) (car aexp)))
+
+(define set?
+  (lambda (lat)
+  (cond
+    ((null? lat) #t)
+    ((member? (car lat) (cdr lat)) #f)
+    (else (set? (cdr lat))))))
+
